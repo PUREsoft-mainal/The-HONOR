1bf0ccfec76f2b5f776066f5400dce2d391071cd
PUREsoft-mainal <manblazman@gmail.com>
2026-06-26 16:22:33 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 // 🌐 إعداد كائن الـ Socket.io المطور والمؤمن سيبرانياً لـ The HONOR
 const io = new Server(server, {
     cors: {
-        origin: ["https://ouro-steps.vercel.app", "https://puresoft-mainal-the-honor.hf.space"],
+        origin: ["https://the-honor.vercel.app/", "https://puresoft-mainal-the-honor.hf.space"],
         methods: ["GET", "POST"],
         credentials: true 
     },
