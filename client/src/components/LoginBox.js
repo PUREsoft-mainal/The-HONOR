08ffa5655cd739f467b9c2edbd0486d8f8b846a4
PUREsoft-mainal <manblazman@gmail.com>
2026-07-01 22:58:29 +0300

    Update LoginBox.js

--- client/src/components/LoginBox.js
+++ client/src/components/LoginBox.js
@@ -12,7 +12,7 @@ const LoginBox = ({ isSignUp, setIsSignUp, user, setUser, password, setPassword,
 
   return (
     <div className="login-box-wrapper">
-    <img src={`${process.env.PUBLIC_URL}/assets/logo.png`} className="main-logo" alt="OURO" style={{ width: '320px', height: 'auto', display: 'block', margin: '0 auto 20px' }} />      <h2 className="login-title">{isSignUp ? "إنشاء حساب ملكي جديد" : "دخول المنصة المحلية"}</h2>
+    <img src={`${process.env.PUBLIC_URL}/assets/logo.png`} className="main-logo" alt="HONOR" style={{ width: '320px', height: 'auto', display: 'block', margin: '0 auto 20px' }} />      <h2 className="login-title">{isSignUp ? "إنشاء حساب ملكي جديد" : "دخول المنصة المحلية"}</h2>
       
       <form onSubmit={onFormSubmit}>
         <div className="login-fields-container">
